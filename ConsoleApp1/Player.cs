f52d34b104d6e825affc15f13cfd18664c686c6d
steelybel <bel.bradberry26@gmail.com>
2019-09-03 15:29:25 -0700

    experimental changes saved

---
+++ ConsoleApp1/Player.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib;
+using rl = Raylib.Raylib;
+
+namespace bel_D20
+{
+    class Player
+    {
+        public Race pcRace;
+        public Class pcClass;
+        public int skinColor;
+        public int[] stats = new int[6];
+        int[] finStats = new int[6];
+        int pStr;
+        int pDex;
+        int pCon;
+        int pInt;
+        int pWis;
+        int pCha;
+        public int wepLevel;
+        Rectangle skin
+        {
+            get
+            {
+                return pcRace.skin;
+            }
+        }
+        Rectangle hair
+        {
+            get
+            {
+                return pcRace.hair;
+            }
+        }
+        Rectangle hair2
+        {
+            get
+            {
+                return pcRace.hair2;
+            }
+        }
+        public Rectangle hat;
+        public Rectangle top;
+        public Rectangle bottom;
+        public Rectangle shoe;
+        public Rectangle shield;
+        public Rectangle wep;
+        public void Draw(Texture2D spr, Vector2 pos)
+        {
+            Vector2 posf = new Vector2(pos.x - 8, pos.y - 8);
+            rl.DrawTextureRec(spr,skin,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,bottom,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,shoe,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,top,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,hair2,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,hair,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,hat,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,shield,posf,Color.WHITE);
+            rl.DrawTextureRec(spr,wep,posf,Color.WHITE);
+        }
+    }
+    class Race
+    {
+        //cosmetic
+        public Rectangle skin;
+        public Rectangle hair;
+        public Rectangle hair2;
+        //stats
+        public int[] skillMod = new int[6];
+    }
+    //RACES
+    class Elf_m : Race
+    {
+        public Elf_m()
+        {
+            skin = new Rectangle(0, 0, 16, 16);
+            hair = new Rectangle(358, 85, 16, 16);
+        }
+    }
+    class Elf_f : Race
+    {
+        public Elf_f()
+        {
+            skin = new Rectangle(17, 0, 16, 16);
+            hair = new Rectangle(358, 85, 16, 16);
+        }
+    }
+    class Dwarf_m : Race
+    {
+        public Dwarf_m()
+        {
+            skin = new Rectangle(0, 0, 16, 16);
+            hair = new Rectangle(443, 17, 16, 16);
+            hair2 = new Rectangle(426, 51, 16, 16);
+        }
+    }
+    class Dwarf_f : Race
+    {
+        public Dwarf_f()
+        {
+            skin = new Rectangle(17, 0, 16, 16);
+            hair = new Rectangle(443, 17, 16, 16);
+            hair2 = new Rectangle(426, 51, 16, 16);
+        }
+    }
+    //CLASSES
+    class Class
+    {
+        //cosmetic
+        public Rectangle hat;
+        public Rectangle top;
+        public Rectangle bottom;
+        public Rectangle shoe;
+        public Rectangle shield;
+        public Rectangle wep;
+        //stats
+    }
+    class Wizard : Class
+    {
+        public Wizard()
+        {
+            hat = new Rectangle(494, 136, 16, 16);
+            top = new Rectangle(256, 119, 16, 16);
+            wep = new Rectangle(715, 17, 16, 16);
+        }
+    }
+    //premade chars
+    class Premade1 : Player
+    {
+        public Premade1()
+        {
+            pcRace = new Elf_f();
+            pcClass = new Wizard();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -23,12 +23,20 @@ namespace bel_D20
             Texture2D hero = rl.LoadTexture("Resources/Sprites/roguelikeChar_transparent.png");
             Rectangle tile = new Rectangle(0, 0, 16, 16);
             Rectangle skin1 = new Rectangle(0, 0, 16, 16);
+            Rectangle skin1f = new Rectangle(17, 0, 16, 16);
             Rectangle skin2 = new Rectangle(0, 17, 16, 16);
+            Rectangle skin2f = new Rectangle(17, 17, 16, 16);
             Rectangle skin3 = new Rectangle(0, 34, 16, 16);
+            Rectangle skin3f = new Rectangle(17, 34, 16, 16);
             Rectangle skin4 = new Rectangle(0, 51, 16, 16);
+            Rectangle skin4f = new Rectangle(17, 51, 16, 16);
+            Rectangle hairElf = new Rectangle(358, 85, 16, 16);
             Rectangle shirt1 = new Rectangle(103, 0, 16, 16);
-            Rectangle mBarbTop = new Rectangle(103, 17, 16, 16);
-            Rectangle fBarbTop = new Rectangle(120, 17, 16, 16);
+            Rectangle TopElf = new Rectangle(120, 119, 16, 16);
+            Rectangle TopWiz = new Rectangle(256, 119, 16, 16);
+            Rectangle mTopBarb = new Rectangle(103, 17, 16, 16);
+            Rectangle fTopBarb = new Rectangle(120, 17, 16, 16);
+            Rectangle hatWiz = new Rectangle(494, 136, 16, 16);
             Rectangle pants1 = new Rectangle(52, 0, 16, 16);
             Rectangle pants2 = new Rectangle(52, 17, 16, 16);
             Rectangle pants3 = new Rectangle(52, 34, 16, 16);
@@ -45,7 +53,25 @@ namespace bel_D20
             Rectangle shoe6 = new Rectangle(69, 102, 16, 16);
             Rectangle shoe7 = new Rectangle(69, 119, 16, 16);
             Rectangle shoe8 = new Rectangle(69, 136, 16, 16);
-            Vector2 playerPos = new Vector2(400, 300);
+            Rectangle sword1 = new Rectangle(749, 102, 16, 16);
+            Rectangle knife1 = new Rectangle(749, 119, 16, 16);
+            Rectangle staff1 = new Rectangle(715, 17, 16, 16);
+            Rectangle axe1 = new Rectangle(800, 17, 16, 16);
+            Vector2 hero1Pos = new Vector2(352, 300);
+            Vector2 hero2Pos = new Vector2(384, 300);
+            Vector2 hero3Pos = new Vector2(416, 300);
+            Vector2 hero4Pos = new Vector2(448, 300);
+
+            
+            
+
+            Player hero1 = new Player();
+
+            Player hero2 = new Player();
+
+            Player hero3 = new Player();
+
+            Player hero4 = new Player();
             //--------------------------------------------------------------------------------------
 
             // Main game loop
@@ -63,10 +89,10 @@ namespace bel_D20
                 rl.ClearBackground(Color.RAYWHITE);
 
                 rl.DrawText("Congrats! You rolled a " + dice.d20(1), 190, 200, 20, Color.LIGHTGRAY);
-                rl.DrawTextureRec(hero, skin1, playerPos, Color.WHITE);
-                rl.DrawTextureRec(hero, mBarbTop, playerPos, Color.WHITE);
-                //rl.DrawTextureRec(hero, pants8, playerPos, Color.WHITE);
-                rl.DrawTextureRec(hero, shoe1, playerPos, Color.WHITE);
+                hero1.Draw(hero, hero1Pos);
+                hero2.Draw(hero, hero2Pos);
+                hero3.Draw(hero, hero3Pos);
+                hero4.Draw(hero, hero4Pos);
                 rl.EndDrawing();
                 //----------------------------------------------------------------------------------
             }
